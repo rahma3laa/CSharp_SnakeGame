1e2667c53c678e41c5c7ec5c4fa44ce088a34e1b
rahma3laa <rahmaaaliii55@gmail.com>
2024-11-10 19:46:24 +0200

    123

---
+++ Coord.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Coord
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+                return false;
+            Coord coord = obj as Coord;
+            return x == coord.x && y == coord.y;
+            
+        }
+        public void applyMovementDirections(Directions directions)
+        {
+            switch (directions) { 
+             case Directions.Left:
+                    x--;
+                    break;
+             case Directions.Right:
+                    x++;
+                    break;
+             case Directions.Up:
+                    y--;
+                    break;
+             case Directions.Down:
+                    y++;
+                    break;
+
+
+            }
+        }
+
+    }
+}
